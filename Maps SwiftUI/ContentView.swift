7d5860d5a745696d8119703181c473ce14b35a2d
Álvaro Ferrández <alvaro.ferrandez.dev@gmail.com>
2021-06-17 10:02:01 +0200

    Initial Commit

---
+++ Maps SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maps SwiftUI
+//
+//  Created by Álvaro Ferrández Gómez on 17/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maps SwiftUI/Maps_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maps_SwiftUIApp.swift
+//  Maps SwiftUI
+//
+//  Created by Álvaro Ferrández Gómez on 17/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Maps_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
